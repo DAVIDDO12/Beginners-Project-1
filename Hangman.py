5cb9319604539699e78868ec044d8aa75826e964
DAVIDDO12 <168615558+DAVIDDO12@users.noreply.github.com>
2024-08-13 17:02:33 +1200

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -12,3 +12,32 @@ HMpics = ["  +---+\n  |   |\n      |\n      |\n      |\n      |\n=========",
         "  +---+\n  |   |\n  O   |\n /|\  |\n /    |\n      |\n=========",
 
         "  +---+\n  |   |\n  O   |\n /|\  |\n / \  |\n      |\n========="]
+words = ["code", "python", "hangman", "programming", "computer"]
+def hangman():
+    word = random.choice(words)
+    guessedletters = []
+    correctletters = [''] * len(word)
+    attempts = 0
+    maxattempts = len(HMpics) - 1
+    while attempts < maxattempts and ''.join(correctletters) != word:
+        print(HMpics[attempts])
+        print(' '.join(correctletters))
+        print("Letters guessed:", ' '.join(guessedletters))
+        guess = input("Guess a letter: ").lower()
+        if guess in guessedletters:
+            print("You already guessed that!")
+            continue
+        guessedletters.append(guess)
+        if guess in word:
+            for i in range(len(word)):
+                if word[i] == guess:
+                    correctletters[i] = guess
+        else:
+            print("Nope")
+            attempts += 1
+    if ''.join(correctletters) == word:
+        print("You got it! The word was:", (word))
+    else:
+        print(HMpics[attempts])
+        print("He died rip 😦 The word was:", (word))
+hangman()
